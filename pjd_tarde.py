ea43164409a2ee9f4be01c1a49ebdf3642726693
Arthur Santos <santosteixeira2807@gmail.com>
2025-10-22 20:19:35 +0000

    Python-codigos

---
+++ pjd_tarde.py
@@ -0,0 +1,48 @@
+"""
+# 1) IMPRIMA SEU NOME
+
+nome = "Arthur Teixeira"
+idade = 17
+print(nome,idade)
+
+# 2) IMPRIMA O NOME DA UNIDADE DO SENAI E SUA IDADE
+
+unidade = Senai
+idade = 83
+print(nome,idade,unidade)
+
+a = 10
+b = 5
+print("A soma é:", a + b)
+
+#3) IMPRIMA A SOMA DA SUA IDADE COM O ANO DO SEU NASCIMENTO
+
+name = input("Digite o seu nome: ")
+print(name)
+
+#4  
+
+idade = int(input("Dgite sua idade: "))
+if idade > 18:
+    print("Voce é de maior")
+else:
+    print("Voce é menor")
+
+#5) CRIE UM CODIGO EM QUE SE A PESSOA NASCEU DEPOIS DOS ANOSS 2000 É GERAÇAO Z
+
+for i in range(5):
+    print("Contador: ", i) 
+
+#6) FAÇA UM CODIGO EM QUE IMPRIMA TODOS OS VALORES PARES DE 0 A 10
+
+def saudacao(nome):
+    print("Bem vindo,", nome)
+
+saudacao("Lucas")
+"""
+#7) CRIE A FUNÇAO QUE RECEBE DOIS NUMEROS E RETORNA A SOMA DELES
+
+import random
+
+cores = ["azul", "verde", "vermelho", "rosa"]
+cores_
